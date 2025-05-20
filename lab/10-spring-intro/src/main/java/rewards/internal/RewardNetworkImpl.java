652d36fbef6450917bed36a28477e68925609f60
Lan Nguyen Vu <lanvunguyen1101@gmail.com>
2025-05-20 22:55:35 +0700

    feat(*): push solution study

--- lab/10-spring-intro/src/main/java/rewards/internal/RewardNetworkImpl.java
+++ lab/10-spring-intro/src/main/java/rewards/internal/RewardNetworkImpl.java
@@ -1,9 +1,13 @@
 package rewards.internal;
 
+import common.money.MonetaryAmount;
+import rewards.AccountContribution;
 import rewards.Dining;
 import rewards.RewardConfirmation;
 import rewards.RewardNetwork;
+import rewards.internal.account.Account;
 import rewards.internal.account.AccountRepository;
+import rewards.internal.restaurant.Restaurant;
 import rewards.internal.restaurant.RestaurantRepository;
 import rewards.internal.reward.RewardRepository;
 
@@ -52,7 +56,15 @@ public class RewardNetworkImpl implements RewardNetwork {
 	public RewardConfirmation rewardAccountFor(Dining dining) {
 		// TODO-07: Write code here for rewarding an account according to
 		//          the sequence diagram in the lab document
+		Account account = accountRepository.findByCreditCard(dining.getCreditCardNumber());
+		Restaurant restaurant = restaurantRepository.findByMerchantNumber(dining.getMerchantNumber());
+
+		MonetaryAmount amount = restaurant.calculateBenefitFor(account,dining);
+		AccountContribution contribution = account.makeContribution(amount);
+
+		accountRepository.updateBeneficiaries(account);
+
 		// TODO-08: Return the corresponding reward confirmation
-		return null;
+		return rewardRepository.confirmReward(contribution,dining);
 	}
 }
