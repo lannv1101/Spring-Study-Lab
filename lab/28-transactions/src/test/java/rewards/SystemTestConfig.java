f1794bce0cb87a005386f299d018c3f372820641
Lan Nguyen Vu <lanvunguyen1101@gmail.com>
2025-06-04 22:55:24 +0700

    study(28-transaction): add solution lab file

--- lab/28-transactions/src/test/java/rewards/SystemTestConfig.java
+++ lab/28-transactions/src/test/java/rewards/SystemTestConfig.java
@@ -5,9 +5,12 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 import config.RewardsConfig;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
 
 
 @Configuration
@@ -32,5 +35,9 @@ public class SystemTestConfig {
 	//	TODO-02: Define a bean named 'transactionManager' that configures a
 	//           DataSourceTransactionManager.
 	//           How does it know which dataSource to manage?
+	@Bean
+	public PlatformTransactionManager transactionManager(){
+		return new DataSourceTransactionManager(dataSource());
+	}
 	
 }
